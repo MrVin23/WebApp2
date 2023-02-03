f357d13a24a62a05680e0d60ee7c12a71eab1446
Vincent Pieterse <Vincent.pieterse@s4.co.za>
2023-02-03 12:44:34 +0200

    Updated Python to 23

--- web.py
+++ web.py
@@ -6,16 +6,17 @@ from colorama import Fore
 
 # ======================================================================
 #   Instantiating Functions
-# ====================================================================== 
+# ======================================================================
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
     # print(Fore.RED + todo + Fore.WHITE)
-    
+
+
 # ======================================================================
 #   Instantiating Variabls and lists
-# ======================================================================     
+# ======================================================================
 todos = functions.get_todos()
 
 st.title("My Todo App")
@@ -24,19 +25,19 @@ st.write("This is to increase your productivity")
 
 # ======================================================================
 #   Check Box
-# ====================================================================== 
-for index, todo in enumerate (todos):
-    checkbox = st.checkbox(todo, key =todo)
+# ======================================================================
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-        
+
 st.text_input(label="",
               placeholder="Enter a todo",
               on_change=add_todo,
-              key= 'new_todo')
+              key='new_todo')
 
 print("Hello")
 st.session_state

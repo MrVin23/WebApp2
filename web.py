9998e6c9b7a187c2e02942c078583a480674d74d
Vincent Pieterse <Vincent.pieterse@s4.co.za>
2023-02-03 11:49:56 +0200

    App deployment

--- web.py
+++ web.py
@@ -25,9 +25,14 @@ st.write("This is to increase your productivity")
 # ======================================================================
 #   Check Box
 # ====================================================================== 
-for todo in todos:
-    st.checkbox(todo)
-
+for index, todo in enumerate (todos):
+    checkbox = st.checkbox(todo, key =todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+        
 st.text_input(label="",
               placeholder="Enter a todo",
               on_change=add_todo,
